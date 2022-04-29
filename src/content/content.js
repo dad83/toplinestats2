e07bc521468bdf3b827e16c83287f26c1e7da36f
dad83 <74124001+dad83@users.noreply.github.com>
2022-04-29 14:19:09 -0700

    Update content.js

--- src/content/content.js
+++ src/content/content.js
@@ -4,7 +4,7 @@ export const articles = [
             subtitle: `The rise of fantasy baseball led to a more analytics-driven baseball world. But what can scoring systems tell us about the value of players as they compare to traditional metrics?`,
             author: `Devon Andrew Davila`,
             category: "Baseball",
-            date: "October 17, 2021",
+            date: "April 25, 2022",
             thumbnail: "https://toplinestats.com/bwarfantasy.png",
             body: [
                 {
@@ -48,79 +48,8 @@ export const articles = [
                     content: `-Strikeout - minus 1 point`,
                 },
                 {
-                    type: "image",
-                    content: "https://toplinestats.com/statescolumns.png",
-                },
-                {
-                    type: "paragraph",
-                    content: `Existing literature and data in the field of political science will tell you that many of these fields, namely ethnic and educational information, is highly predictive of voting patterns. But will it work on a statewide level?`,
-                },
-                {
-                    type: "paragraph",
-                    content: `Let's clarify something before we go further- the definition of "red" and "blue" states can be subject to interpretation- some reliably liberal states elect Republican governors, and vice versa. So for now, let's define "red" and "blue" as how states tend to vote in presidential elections.`
-                },
-                {
-                    type: "paragraph-link",
-                    content: [
-                        `There are a couple of important data preparation steps involved here. I'll be following the `,
-                        {
-                            text: `Real Python`,
-                            link: `https://realpython.com/k-means-clustering-python/`,
-                        },
-                        ` methodology here for scaling, determining the ideal number of components, and utilizing their K-means clustering pipeline.`
-                    ],
-                },
-                {
                     type: "text",
-                    content: `First, the data must be scaled.`,
-                },
-                {
-                    type: "image",
-                    content: "https://toplinestats.com/scaledstates.png",
-                },
-                {
-                    type: "text",
-                    content: `The new scaled_states array would be a bit boring to look at, so let's skip ahead to figuring out how many components we need for this analysis:`,
-                },
-                {
-                    type: "image",
-                    content: "https://toplinestats.com/stateclusterimg3.png",
-                },
-                {
-                    type: "text",
-                    content: "Looks like we'll need 11 components to explain 95% of the variance in the data. With that, we should now be able to define the Realy Python PCA pipeline and run the analysis:",
-                },
-                {
-                    type: "image",
-                    content: "https://toplinestats.com/stateclusterimg4.png",
-                },
-                {
-                    type: "image",
-                    content: "https://toplinestats.com/stateclusterimg5.png",
-                },
-                {
-                    type: "text",
-                    content: `I've inverted the x axis here for a bit of visualization help, with blue states on the left and red on the right. But without knowing which states are which, this is pretty meaningless. So let's head to Tableau-`
-                },
-                {
-                    type: "image",
-                    content: "https://i.ibb.co/F8Cm898/stateclusterimg6.png",
-                },
-                {
-                    type: "paragraph",
-                    content: `Here's a simple plot of the states by principal components 1 and 2, where the size of each dot represents the number of electoral votes for each state. The one noteworthy outlier I could find is New Mexico, which has reliably voted for Democratic presidential candidates each election since 2004. Previously, it was considered a swing state.`
-                },
-                {
-                    type: "text",
-                    content: `So how well does this fare against results from the most recent presidential election?`
-                },
-                {
-                    type: "paragraph",
-                    content: `The model scored misses on Wisconsin, Nevada, Michigan, Arizona, Georgia, New Mexico, and Wisconsin, all of which went blue when the model predicted red (and all of which, save for New Mexico, were considered swing states). That yeilds an accuracy rate of 43/50, or 86%. Not bad!`
-                },
-                {
-                    type: "paragraph",
-                    content: `So what, if anything, does this model tell us? My read is that, if the model is to be trusted, Republicans seem to have a bit of room to grow into untapped territory. Specifically, I would be curious to know why the model misidentified New Mexico- is this an opportunity for Republicans? Maybe with the right candidate they can thrive in the states identified here.`
+                    content: `TO BE CONTINUED`,
                 },
 
             ],
@@ -130,7 +59,7 @@ export const articles = [
             subtitle: `A cluster analysis of states based on different metrics like poverty rate, union membership, and religious demographics.`,
             author: `Devon Andrew Davila`,
             category: "Politics",
-            date: "October 29, 2021",
+            date: "April 15, 2022",
             thumbnail: "https://toplinestats.com/stateclusterimg1.png",
             body: [
                 {
